e669b1e1d6707ef73ba8c56578b6536262a137ac
alex_fajar_tri_utomo <alex.fajar10@gmail.com>
2021-05-03 17:44:25 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4007/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if4007/galerihewan/model/Hewan.kt
@@ -1,8 +1,10 @@
 package org.d3if4007.galerihewan.model
 
+import org.d3if4007.galerihewan.R
+
 data class Hewan (
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
 
 )
